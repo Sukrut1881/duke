7a9cd4ab35e7e8fb11cb45484ddbda222ae2a014
Sukrut1881 <e0311151@u.nus.edu>
2019-08-23 16:50:15 +0800

    Done check lvl3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class Duke {
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,30 +24,42 @@ public class Duke {
 
         System.out.println(initial);
 
-        String [] string_array = new String[100];
+        Task [] string_array = new Task[100];
 
         Scanner input = new Scanner(System.in);
         String input1 = input.nextLine();
         int counter = 0;
 
-        while ( !(input1.equals("bye")))
+        while (!(input1.equals("bye")))
         {
             if ( input1.equals("list"))
             {
                 System.out.println("    ____________________________________________________________\n" );
                 for (int i = 0; i < counter; i++ )
                 {
-                    System.out.println("\t"+ i + "." + " " + string_array[i] + "\n");
+                    int s = i+1;
+                    System.out.println("\t"+ s + "." + " [" + string_array[i].getStatusIcon()+ "] "+ string_array[i].description + "\n");
                 }
                 System.out.println ("    ____________________________________________________________");
             }
+            else if (input1.contains("done ") && isInteger(String.valueOf(input1.charAt(5))))
+            {
+                int location = input1.charAt(5)-'0'-1;
+//                System.out.println(location);
+                string_array[location].markAsDone();
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Nice! I've marked this task as done: \n" +
+                        "       [" + string_array[location].getStatusIcon()+ "]" + string_array[location].description +  "\n" +
+                        "    ____________________________________________________________");
+            }
             else
             {
-                string_array[counter] = input1;
-                counter++;
+                Task t = new Task(input1);
+                string_array[counter]= t;
                 System.out.println("    ____________________________________________________________\n" +
-                        "     added: "+ input1+ "\n" +
+                        "     added: "+ " [" + string_array[counter].getStatusIcon()+ "] " +string_array[counter].description+ "\n" +
                         "    ____________________________________________________________");
+                counter++;
             }
             input1 = input.nextLine();
         }
